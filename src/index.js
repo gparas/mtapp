e38d7d776199e13046deb1342f84d11735832ca9
gparas <parageos@gmail.com>
2018-09-14 22:05:17 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from './pages/Home';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Index />, document.getElementById('root'));
-registerServiceWorker();
